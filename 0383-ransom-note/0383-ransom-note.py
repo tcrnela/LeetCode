e12ecee163aeacfbe59e383e247c178522b7b5e1
tcrnela <tcrnela@gmail.com>
2026-02-01 22:27:23 +0900

    Time: 32 ms (9.86%), Space: 19.7 MB (34.77%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,12 +2,11 @@ from collections import defaultdict
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ans = True
         b = defaultdict(int)
         for i in range (len(magazine)):
             b[magazine[i]] += 1
         for i in range (len(ransomNote)):
             b[ransomNote[i]] -= 1
             if b[ransomNote[i]] < 0:
-                ans = False
-        return ans
+                return False
+        return True
