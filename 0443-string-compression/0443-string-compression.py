1bc78d7cfdefee044b1bf80c97651963ca5aaacd
tcrnela <tcrnela@gmail.com>
2026-02-22 19:37:17 +0900

    Time: 4 ms (14.63%), Space: 19.2 MB (77.71%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,19 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        chars.append("")
+        ex = chars[0]
+        cnt = 0
+        p = 0
+        for i in (chars):
+            if i == ex:
+                cnt += 1
+            else:
+                chars[p] = ex
+                p += 1
+                if cnt > 1:
+                    for j in range (len(str(cnt))):
+                        chars[p] = str(cnt)[j]
+                        p += 1
+                    cnt = 1
+                ex = i
+        return p
