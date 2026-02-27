32b78f0ede15f8ed771db37d0aec731369781aaf
tcrnela <tcrnela@gmail.com>
2026-02-27 19:17:27 +0900

    Time: 84 ms (37.61%), Space: 22 MB (8.36%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -4,11 +4,9 @@ class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
-
-        ans = float("inf")
+    
         vis = set()
         q = deque()
-        flag = 0
 
         r = ["+"] * (len(maze[0]) + 2)
         maze = [r] + [["+"] + row + ["+"] for row in maze] + [r]
@@ -21,12 +19,10 @@ class Solution:
         while(q):
             (y, x, z) = q.popleft()
             if (y, x) != tuple(entrance) and (y == 1 or y == len(maze) - 2 or x == 1 or x == len(maze[0]) - 2):
-                if ans > z:
-                    ans = z
-                    flag = 1
+                return z
             for i in range (4):
                 if maze[y + dy[i]][x + dx[i]] == "." and (y + dy[i], x + dx[i]) not in vis:
                     vis.add((y + dy[i], x + dx[i]))
                     q.append((y + dy[i], x + dx[i], z + 1))
-        if flag == 0: return -1
-        return ans
+
+        return -1
