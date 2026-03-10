913a2f8cce16d8dd28dde6e98c74f9c1b42a436d
tcrnela <tcrnela@gmail.com>
2026-03-10 23:24:33 +0900

    Time: 0 ms (100%), Space: 19.3 MB (72.19%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0] * (n+1) for _ in range (m)]
+
+        for i in range (1, n+1):
+            grid[0][i] = 1
+        for i in range (1, m):
+            for j in range (1, n+1):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[m-1][n]
